a4daf530b4597d43f523d4622e167be4dbf65957
José F. Romaniello <jfromaniello@ubuntu.(none)>
2011-09-30 20:57:46 -0700

    first attempt

--- test.js
+++ test.js
@@ -1,12 +1,50 @@
-var express = require('express');
+var express = require('express'),
+	path = require("path"),
+	md = require('node-markdown').Markdown,
+	hbs = require("hbs"),
+	fs = require('fs'),
+	util = require('./util.js');
 
+//var html = md("hello \n====\nthis is something important");
+var slidesDir = __dirname + "/slides/";
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.configure(function(){
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'hbs');
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(app.router);
+	app.use(express.static(__dirname + '/public'));
 });
 
+app.get('/', function(request, response) {
+	response.render("index");
+});
+
+app.post('/', function(request, response){
+	console.log("the markdown is " +  request.body.slideMarkdown);
+	var fileName = util.randomString() ;
+	fs.writeFile(slidesDir + fileName + ".markdown", request.body.slideMarkdown, function(err){
+		if(!err)
+			response.redirect("/slide/" + fileName)
+		else
+			response.send(err, 500);  
+	});  
+});
+
+app.get('/slide/:file', function(request, response){
+	fs.readFile(slidesDir + request.params.file + ".markdown", function(err, data){
+		if(err) throw err;
+		console.log("the markdown is " +  data.toString());
+		var html = md(data.toString());
+		response.send(html);
+	});
+});
+
+
 var port = process.env.PORT || 3000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
---
+++ util.js
@@ -0,0 +1,10 @@
+exports.randomString = function() {
+	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+	var string_length = 8;
+	var randomstring = '';
+	for (var i=0; i<string_length; i++) {
+		var rnum = Math.floor(Math.random() * chars.length);
+		randomstring += chars.substring(rnum,rnum+1);
+	}
+	return randomstring;
+};
